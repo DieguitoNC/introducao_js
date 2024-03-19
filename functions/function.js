a3fa8ed565586e8805dba4b83a6f5dcb74530834
DieguitoNC <diegocatalann@gmail.com>
2024-03-18 21:48:44 -0300

    estudo ate function

---
+++ functions/function.js
@@ -0,0 +1,11 @@
+
+// Function expression
+// Function anonymous
+
+// Parâmetros (parameters)
+const sum = function(number1, number2) {
+  let x = number1 + number2
+  console.log(x)
+}
+
+sum(2,3) // arguments - argumentos
---
+++ functions/function1.js
@@ -0,0 +1,17 @@
+//Criar um aplicatio de frases motivacionais
+
+
+// Declaration - Declaração da função
+// function statement
+function createPhrases () {
+  console.log('Estudar é muito bom')
+  console.log('Paciência e persistência')
+  console.log('Revisão é a mãe do aprendizado')
+}
+
+// Executar a função
+// Execute, run, call, invoke
+
+createPhrases()
+
+console.log('Fim do programa')
